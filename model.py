dd760ac80dbafcf3c76fd65da89ef3d5c9b43518
kenobijr <kenobijr@tatooine.com>
2025-06-21 16:50:28 +0200

    completed some testruns with acceptable nlll; some project structuring

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as Optim
+import random
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
